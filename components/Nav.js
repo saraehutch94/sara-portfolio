598153abe5eddad358f87ec3a7d37ece831f3e90
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-30 21:42:21 -0400

    add computer animation to portfolio home page

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 function Nav() {
     return (
         <nav className="bg-slate-600 h-14 w-full">
-            <div className="flex justify-end items-center h-full space-x-8 mr-8">
+            <div className="flex sm: justify-end md:justify-center items-center h-full sm: space-x-10 md:space-x-20 mr-8">
                 <Link href="/" passHref><svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 cursor-pointer text-green-500 hover:text-green-400" viewBox="0 0 20 20" fill="currentColor"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" /></svg></Link>
                 <Link href="/about" passHref><svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 cursor-pointer text-green-500 hover:text-green-400" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" /></svg></Link>
                 <Link href="/projects" passHref><svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 cursor-pointer text-green-500 hover:text-green-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" /></svg></Link>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,34 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Meta from '../components/Meta'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <>
+        <Meta keywords="software developer, software engineer, web developer, programming, programmer, React, Boston, S3H" description="My name is Sara Hutchinson, a full-stack software developer based out of the Greater Boston area of Massachusetts. Check out my work and reach out!" title='<S3H>'/>
+        <div className={styles.flex}>
+          <div className={styles.main}>
+            <div className={styles.computerwrapper}>
+              <div className={styles.wholecomputer}>
+                <div className={styles.monitortop}>
+                  <div className={styles.monitortext}>
+                      <div className={styles.animation1}>const S3H = &#123;</div>
+                      <div className={styles.animation2}>firstName: &quot;Sara&quot;,</div>
+                      <div className={styles.animation3}>lastName: &quot;Hutchinson&quot;,</div>
+                      <div className={styles.animation4}>location: &quot;Boston, MA&quot;,</div>
+                      <div className={styles.animation5}>isA: &quot;Software Developer&quot;,</div>
+                      <div className={styles.animation6}>message: &quot;Welcome to my Portfolio!&quot;,</div>
+                      <div className={styles.animation7}>&#125;;</div>
+                  </div>
+              </div>
+              <div className={styles.monitorbottom}>
+                  <div className={styles.logo}>&lt;S3H&gt;</div>
+              </div>
+              <div className={styles.monitorneck}></div>
+              <div className={styles.monitorstand}></div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </>
   )
 }
