6acc353fd3ed7ac3e26ff40123aafaed8457de28
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-30 22:28:35 -0400

    add google font / more styling to homepage

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['i.imgur.com'],
+  },
+}
