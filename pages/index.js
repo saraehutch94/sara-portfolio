f57898999919e4f6b729cbaa2356977ca337db1c
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-31 13:32:48 -0400

    med-screen and mobile versions of homepage

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,17 @@ export default function Home() {
         <Meta keywords="software developer, software engineer, web developer, programming, programmer, React, Boston, S3H" description="My name is Sara Hutchinson, a full-stack software developer based out of the Greater Boston area of Massachusetts. Check out my work and reach out!" title='<S3H>'/>
         <div className={styles.flex}>
           <div className={styles.main}>
-            <div className="flex space-x-40">
-              <div className="flex flex-col justify-center align-center relative bottom-16">
-                <div className="flex flex-col space-y-2">
-                  <div className={styles.introtext}>welcome</div>
-                  <div className={styles.introparawrapper}>
-                    <div className={styles.intropara}>I am a software developer based</div>
-                    <div className={styles.intropara}>out of Boston, MA. Check out my</div>
-                    <div className={styles.intropara}>work above and reach out for</div>
-                    <div className={styles.intropara}>inquiries!</div>
+            <div className={styles.flexcontent}>
+              <div className={styles.welcomecontent}>
+                <div className={styles.wholeintrotext}>
+                  <div className="flex flex-col space-y-2">
+                    <div className={styles.introtext}>welcome</div>
+                    <div className={styles.introparawrapper}>
+                      <div className={styles.intropara}>I am a software developer based</div>
+                      <div className={styles.intropara}>out of Boston, MA. Check out my</div>
+                      <div className={styles.intropara}>work above and reach out for</div>
+                      <div className={styles.intropara}>inquiries!</div>
+                    </div>
                   </div>
                 </div>
               </div>
