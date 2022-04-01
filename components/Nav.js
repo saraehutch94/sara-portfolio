6bbeceb6f598ee36617cd40780434e8d46fddbc4
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-01 11:07:52 -0400

    change color scheme of app

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import LogoNav from './LogoNav'
 import { useState } from 'react'
+import styles from '../styles/Nav.module.css'
 
 function Nav() {
 
@@ -12,7 +13,7 @@ function Nav() {
 
     return (
         <>
-        <nav className="bg-slate-600 h-14 w-full">
+        <nav className={styles.navfull}>
             <div className="hidden md:flex md:justify-center items-center h-full lg:space-x-20 space-x-10">
                 <Link href="/" passHref><svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 cursor-pointer text-green-500 hover:text-green-400" viewBox="0 0 20 20" fill="currentColor"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" /></svg></Link>
                 <Link href="/about" passHref><svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 cursor-pointer text-green-500 hover:text-green-400" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" /></svg></Link>
