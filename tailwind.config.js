e2148cb4a83e647361fd68de7bd08ef55ef1ddcd
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-01 12:13:16 -0400

    fix nav bar colors to match new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'navDarkGreen': '#0D2823',
+      'navLightGreenHover': '#184a41',
+      'navIconReg': '#22C55D',
+      'navIconHover': '#49DE80',
+      'greenBody': '#123A33',
+    },
     extend: {
       fontFamily: {
         sans: ['Rubik'],
