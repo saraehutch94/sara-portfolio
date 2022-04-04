c77997af701f7ac58baffc93f8984531a7b1d5ea
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-04 17:15:47 -0400

    mobile responsive about me page

--- pages/about.js
+++ pages/about.js
@@ -8,20 +8,25 @@ function About() {
             <NavPages />
             <div className="min-h-screen flex flex-col font-sans">
                 <div className="h-screen grow flex flex-col justify-center items-center">
-                    <div className="flex justfy-center items-center">
-                        <div className="w-8/12 space-y-3.5">
-                            <div className="text-6xl text-navIconReg font-medium">about me</div>
-                            <div className="text-white text-3xl">I am a Software Developer from Boston, MA</div>
-                            <div className="text-white text-xl">I enjoy the challenges and brain-stimulation of coding. <br/>Being a part of an industry that is ever-changing and constantly evolving<br />blesses me with frequent opportunities to grow and learn new programming <br />skills. I wake up every day with new knowledge I hadn&apos;t known about the <br />previous day. I am very fond of front-end development, especially <span className="text-navIconReg">React</span>.</div>
-                            <div className="text-white text-xl">I would also consider myself a music connoisseur (check out my <br/><span className="text-navIconReg">Sarify</span> project). Other pastimes of mine are cooking and enjoying nature.</div>
-                            <div className="text-white text-xl">Connect with me on <span className="text-navIconReg">LinkedIn</span> and check out my code on <span className="text-navIconReg">GitHub</span> below!</div>
-                            <div className="flex">
+                    <div className="flex flex-col-reverse xl:flex-row justfy-center items-center xl:mt-0 md:mt-96 sm:mt-[250px] xs:mt-[300px] mt-[500px]">
+                        <div className="xl:w-8/12 xl:text-left text-center space-y-3.5 mt-10 xl:mb-0 mb-10">
+                            <div className="md:text-6xl text-5xl text-navIconReg font-medium">about me</div>
+                            <div className="text-white md:text-3xl text-2xl">I am a Software Developer from Boston, MA</div>
+                            <div className="text-white md:text-xl text-lg md:block hidden">I enjoy the challenges and brain-stimulation of coding.<br/>Being a part of an industry that is ever-changing and constantly evolving<br/>blesses me with frequent opportunities to grow and learn new programming<br/>skills. I wake up every day with new knowledge I hadn&apos;t known about the<br/>previous day. I am very fond of front-end development, especially <span className="text-navIconReg">React</span>.</div>
+                            <div className="text-white text-lg md:hidden">I enjoy the challenges and brain-stimulation of coding. Being a part of an industry that is ever-changing and constantly evolving blesses me with frequent opportunities to grow and learn new programming skills. I wake up every day with new knowledge I hadn&apos;t known about the previous day. I am very fond of front-end development, especially <span className="text-navIconReg">React</span>.</div>
+                            <div className="text-white md:text-xl text-lg md:block hidden">I would also consider myself a music connoisseur (check out my<br/><span className="text-navIconReg">Sarify</span> project). Other pastimes of mine are cooking and enjoying nature.</div>
+                            <div className="text-white text-lg md:hidden">I would also consider myself a music connoisseur (check out my <span className="text-navIconReg">Sarify</span> project). Other pastimes of mine are cooking and enjoying nature.</div>
+                            <div className="text-white md:text-xl text-lg">Connect with me on <span className="text-navIconReg">LinkedIn</span> and check out my code on <span className="text-navIconReg">GitHub</span> below!</div>
+                            <div className="flex xl:justify-start justify-center">
                                 <a href="https://www.linkedin.com/in/sara-hutchinson/" target="_blank" rel="noreferrer"><div className="cursor-pointer"><Image src="https://i.imgur.com/ptiNN7E.png" height="44px" width="54px" alt="linkedin logo"/></div></a>
                                 <a href="https://github.com/saraehutch94" target="_blank" rel="noreferrer"><div className="cursor-pointer"><Image src="https://i.imgur.com/4STVaVZ.png" height="44px" width="54px" alt="github logo"/></div></a>
                             </div>
                         </div>
-                        <div className="photo">
+                        <div className="md:block hidden">
                             <Image src="https://i.imgur.com/sNpTVEq.jpg" width="500px" height="500px" alt="photo of Sara" className="rounded-full" responsive/>
+                        </div>
+                        <div className="md:hidden">
+                            <Image src="https://i.imgur.com/sNpTVEq.jpg" width="350px" height="350px" alt="photo of Sara" className="rounded-full" responsive/>
                         </div>
                     </div>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
       'navIconHover': '#49DE80',
       'greenBody': '#123A33',
     },
+    screens: {
+      'xs': '376px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily: {
         sans: ['Rubik'],
