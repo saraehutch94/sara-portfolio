2de7d61e0ae17a6a23f648aef8caa4895082df79
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-04 21:03:38 -0400

    continue with project page

--- pages/projects.js
+++ pages/projects.js
@@ -7,13 +7,13 @@ function Projects() {
             <Meta keywords="software developer, software engineer, web developer, programming, programmer, React, Boston, S3H" description="My name is Sara Hutchinson, a full-stack software developer based out of the Greater Boston area of Massachusetts. Check out my work and reach out!" title='<S3H> - Projects'/>
             <NavPages />
             <div className="min-h-screen flex flex-col font-sans">
-                <div className="h-screen grow flex flex-col justify-center items-center">
-                    <div className="text-6xl font-medium text-navIconReg">projects</div>
-                    <div className="flex">
-                        <div className="">first project</div>
-                        <div className="">second project</div>
-                        <div className="">third project</div>
-                        <div className="">fourth project</div>
+                <div className="h-full grow flex flex-col justify-center items-center space-y-14  mb-20">
+                    <div className="text-6xl font-medium text-navIconReg mt-20">projects</div>
+                    <div className="flex flex-col space-y-14 justify-center items-center w-full">
+                        <div className="h-80 w-10/12 bg-navDarkGreen">first project</div>
+                        <div className="h-80 w-10/12 bg-navDarkGreen">second project</div>
+                        <div className="h-80 w-10/12 bg-navDarkGreen">third project</div>
+                        <div className="h-80 w-10/12 bg-navDarkGreen">fourth project</div>
                     </div>
                 </div>
             </div>
