acf6645da1f5802a41fe5da0850245bf329ecd3d
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-06 14:27:33 -0400

    finish responsive/mobile version of first project

--- pages/projects.js
+++ pages/projects.js
@@ -12,20 +12,20 @@ function Projects() {
                 <div className="h-full grow flex flex-col justify-center items-center space-y-14 mb-20">
                     <div className="text-6xl font-medium text-navIconReg mt-20 mb-5">projects</div>
                     <div className="flex flex-col space-y-14 justify-center items-center w-full">
-                        <div className="h-80 w-10/12 flex space-x-8 bg-navDarkGreen border-2 border-navIconReg transition-height-width duration-0.25 ease-out hover:h-96 hover:w-11/12 group">
-                            <div className="w-401.33 h-80 relative transition-height-width duration-0.25 ease-out group-hover:h-96 group-hover:w-475">
+                        <div className="2md:h-80 2md:w-10/12 flex 2md:flex-row flex-col space-y-5 2md:space-x-8 2md:justify-start 2md:items-center justify-center items-center bg-navDarkGreen border-2 border-navIconReg 2md:transition-height-width 2md:duration-0.25 2md:ease-out 2md:hover:h-96 2md:hover:w-11/12 group 2md:pb-0 pb-10">
+                            <div className="1xl:w-401.33 2md:w-375 w-386 h-80 relative 1xs:transition-height-width 1xs:duration-0.25 1xs:ease-out 1xs:group-hover:h-96 1xl:group-hover:w-485 1xs:group-hover:w-435">
                                 <Image src="https://i.imgur.com/Ojrc9f5.png?1" layout='fill' objectFit='fill' alt="first-project-screenshot"/>
                             </div>
-                            <div className="flex flex-col justify-center space-y-5">
-                                <div className="text-2xl text-navIconReg transition-font-size duration-0.25 ease-out font-medium group-hover:text-3xl">Marvel Mania</div>
-                                <div className="text-lg w-700 text-white transition-font-size duration-0.25 ease-out group-hover:text-lg">With this search engine application, you can find you favorite Marvel characters along with a description about them and what stories they were involved in. When searching for a character, a request is made to the Marvel API to query for that character&apos;s information. </div>
-                                <div className="flex space-x-5">
-                                    <i className="devicon-html5-plain text-xl text-navIconReg transition-font-size duration-0.25 ease-out group-hover:text-2xl"></i>
-                                    <i className="devicon-css3-plain text-xl text-navIconReg transition-font-size duration-0.25 ease-out group-hover:text-2xl"></i>
-                                    <i className="devicon-javascript-plain text-xl text-navIconReg transition-font-size duration-0.25 ease-out group-hover:text-2xl"></i>
-                                    <i className="devicon-jquery-plain text-xl text-navIconReg transition-font-size duration-0.25 ease-out group-hover:text-2xl"></i>
+                            <div className="flex flex-col 2md:text-left text-center justify-center space-y-5">
+                                <div className="text-2xl text-navIconReg 1xs:transition-font-size 1xs:duration-0.25 1xs:ease-out font-medium 1xs:group-hover:text-3xl">Marvel Mania</div>
+                                <div className="text-lg 1xl:w-700 2lg:w-600 2.0lg:w-550 1lg:w-500 1.0lg:w-450 2md:w-400 w-350 text-white transition-font-size duration-0.25 ease-out group-hover:text-lg">With this search engine application, you can find you favorite Marvel characters along with a description about them and what stories they were involved in. When searching for a character, a request is made to the Marvel API to query for that character&apos;s information. </div>
+                                <div className="flex 2md:justify-start justify-center space-x-5">
+                                    <i className="devicon-html5-plain text-2xl 1xs:text-xl text-navIconReg 1xs:transition-font-size 1xs:duration-0.25 1xs:ease-out 1xs:group-hover:text-2xl"></i>
+                                    <i className="devicon-css3-plain text-2xl 1xs:text-xl text-navIconReg 1xs:transition-font-size 1xs:duration-0.25 1xs:ease-out 1xs:group-hover:text-2xl"></i>
+                                    <i className="devicon-javascript-plain text-2xl 1xs:text-xl text-navIconReg 1xs:transition-font-size 1xs:duration-0.25 1xs:ease-out 1xs:group-hover:text-2xl"></i>
+                                    <i className="devicon-jquery-plain text-2xl 1xs:text-xl text-navIconReg 1xs:transition-font-size 1xs:duration-0.25 1xs:ease-out 1xs:group-hover:text-2xl"></i>
                                 </div>
-                                <div className="text-lg space-x-3 hidden transition-display duration-0.25 ease-out group-hover:flex">
+                                <div className="flex 1xs:text-lg text-xl 2md:justify-start justify-center space-x-3 1xs:hidden 1xs:transition-display 1xs:duration-0.25 1xs:ease-out 1xs:group-hover:flex">
                                     <a href="https://marvel-mania.netlify.app/" target="_blank" rel="noreferrer" className="text-navIconReg font-medium">Live Site</a>
                                     <div className="text-navIconReg font-medium">|</div>
                                     <a href="https://github.com/saraehutch94/GA-project-1" target="_blank" rel="noreferrer" className="text-navIconReg font-medium">Repository</a>
