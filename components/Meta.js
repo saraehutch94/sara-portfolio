79524d68f54f41ddafebe31e30741c9bbde93e50
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 17:05:57 -0400

    add image meta tag

--- components/Meta.js
+++ components/Meta.js
@@ -6,6 +6,7 @@ function Meta(props) {
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='keywords' content={props.keywords} />
             <meta name='description' content={props.description} />
+            <meta property="og:image" content="https://i.imgur.com/sbOI4Wx.png" />
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.ico' />
             <title>{props.title}</title>
