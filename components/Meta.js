aafbb26a1afc95a1482aba3e770e604e96a3c161
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 17:31:39 -0400

    another fix

--- components/Meta.js
+++ components/Meta.js
@@ -6,6 +6,7 @@ function Meta(props) {
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='keywords' content={props.keywords} />
             <meta name='description' content={props.description} />
+            <meta property="og:image" content="../public/portfolio_img.png" />
             <meta property="og:image:secure_url" content="../public/portfolio_img.png" />
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.ico' />
