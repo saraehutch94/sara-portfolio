9fda559dce89ac1490fbdad8febb49c4b1836c7f
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 17:54:11 -0400

    add all four meta tags - see if it works

--- components/Meta.js
+++ components/Meta.js
@@ -6,8 +6,10 @@ function Meta(props) {
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='keywords' content={props.keywords} />
             <meta name='description' content={props.description} />
-            <meta property="og:image" content={props.photo} />
-            <meta property="og:image:secure_url" content={props.photo} />
+            <meta property="og:title" content="The Rock" />
+            <meta property="og:type" content="video.movie" />
+            <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+            <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.ico' />
             <title>{props.title}</title>
