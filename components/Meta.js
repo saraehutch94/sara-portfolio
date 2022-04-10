cc463e4bccc986ea1853f98c8b8578d24e159b7f
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 17:19:06 -0400

    add meta tags...

--- components/Meta.js
+++ components/Meta.js
@@ -6,7 +6,16 @@ function Meta(props) {
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='keywords' content={props.keywords} />
             <meta name='description' content={props.description} />
-            <meta property="og:image" content="https://i.imgur.com/sbOI4Wx.png" />
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://metatags.io/"/>
+            <meta property="og:title" content="Portfolio: Sara Hutchinson"/>
+            <meta property="og:description" content=""/>
+            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+            <meta property="twitter:card" content="summary_large_image"/>
+            <meta property="twitter:url" content="https://metatags.io/"/>
+            <meta property="twitter:title" content="Portfolio: Sara Hutchinson"/>
+            <meta property="twitter:description" content=""/>
+            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.ico' />
             <title>{props.title}</title>
