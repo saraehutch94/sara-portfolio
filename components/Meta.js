63eea894da2198dd4d292409cb4c3ed762c294dd
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 17:59:08 -0400

    add my content to meta tags

--- components/Meta.js
+++ components/Meta.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import photo from '../public/portfolio_img.png'
 
 function Meta(props) {
     return (
@@ -6,10 +7,10 @@ function Meta(props) {
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='keywords' content={props.keywords} />
             <meta name='description' content={props.description} />
-            <meta property="og:title" content="The Rock" />
-            <meta property="og:type" content="video.movie" />
-            <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
-            <meta property="og:image" content="https://m.media-amazon.com/images/I/81W+Rih-KcL._AC_SL1500_.jpg" />
+            <meta property="og:title" content="<S3H>" />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://www.s3h.dev/" />
+            <meta property="og:image" content={photo} />
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.ico' />
             <title>{props.title}</title>
