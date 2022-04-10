933bf703846c006e6555e4d70eaa02261b46b980
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 17:33:58 -0400

    import photo and add to meta tag

--- components/Meta.js
+++ components/Meta.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import photo from '../public/portfolio_img.png'
 
 function Meta(props) {
     return (
@@ -6,8 +7,8 @@ function Meta(props) {
             <meta name='viewport' content='width=device-width, initial-scale=1' />
             <meta name='keywords' content={props.keywords} />
             <meta name='description' content={props.description} />
-            <meta property="og:image" content="../public/portfolio_img.png" />
-            <meta property="og:image:secure_url" content="../public/portfolio_img.png" />
+            <meta property="og:image" content={photo} />
+            <meta property="og:image:secure_url" content={photo} />
             <meta charSet='utf-8' />
             <link rel='icon' href='/favicon.ico' />
             <title>{props.title}</title>
