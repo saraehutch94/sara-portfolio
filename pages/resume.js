66f8097703e1966e175eac3a4c2b83f2f203c877
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 21:08:38 -0400

    fix resume to be mobile responsive

--- pages/resume.js
+++ pages/resume.js
@@ -10,8 +10,8 @@ function Resume() {
             <div className="min-h-screen flex flex-col font-sans">
                 <div className="h-full grow flex flex-col justify-center items-center">
                 <div className="text-6xl font-medium text-navIconReg mt-20 mb-10">resume</div>
-                <div className="mb-16">
-                    <iframe src="https://drive.google.com/file/d/1RyfaPaSDzgQ8TyTFupEq_UolC_wlMSS-/preview" width="640" height="830" allow="autoplay"></iframe>
+                <div className="1sm:w-628 00xs:w-550 w-310 1sm:h-830 00xs:h-752 h-400 mb-16">
+                    <iframe src="https://drive.google.com/file/d/1RyfaPaSDzgQ8TyTFupEq_UolC_wlMSS-/preview" width="100%" height="100%" allow="autoplay"></iframe>
                 </div>
                 </div>
             </div>
