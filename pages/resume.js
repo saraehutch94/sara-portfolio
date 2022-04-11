bdca228d6f742b78a5a3865a8ec7d1f8e752c3a3
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-04-10 20:00:58 -0400

    updated resume added to iframe

--- pages/resume.js
+++ pages/resume.js
@@ -11,7 +11,7 @@ function Resume() {
                 <div className="h-full grow flex flex-col justify-center items-center">
                 <div className="text-6xl font-medium text-navIconReg mt-20 mb-10">resume</div>
                 <div className="mb-16">
-                    <iframe src="https://drive.google.com/file/d/1xIhKHvHJ3xwl5DICTvMu8G-HHg4M7ilp/preview" width="640" height="830" allow="autoplay"></iframe>
+                    <iframe src="https://drive.google.com/file/d/1RyfaPaSDzgQ8TyTFupEq_UolC_wlMSS-/preview" width="640" height="830" allow="autoplay"></iframe>
                 </div>
                 </div>
             </div>
